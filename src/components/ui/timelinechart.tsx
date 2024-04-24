01b97ce21db87d1edae8215d1be9fa6d22500ca5
Mkhs <jonbecher@microagenetworks.com>
2024-04-24 03:28:11 -0400

    Updated tsx dependency for chart.js

--- src/components/ui/timelinechart.tsx
+++ src/components/ui/timelinechart.tsx
@@ -1,3 +1,3 @@
 //import type {ChartData as PureChartData, ChartOptions} from 'chart.js'
 import React, {PureComponent} from 'react'
-//import {ChartData, Line} from 'react-chartjs-2'
+import {Line} from 'react-chartjs-2'
