c8c0baaf4a2fa043f913eb1a2f834f19c60fe51b
Mkhs <jonbecher@microagenetworks.com>
2024-04-25 19:04:41 -0400

    Set up Rewind-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/@rewind-ui/core/dist/theme/styles/*.js'
   ],
   theme: {
     extend: {
@@ -15,6 +16,12 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwind-scrollbar')({ nocompatible: true }),
+    require('@tailwindcss/forms')({
+      strategy: 'class' // only generate classes
+    })
+  ],
 };
 export default config;
