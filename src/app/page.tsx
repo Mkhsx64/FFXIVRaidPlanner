dbd9d9b8834fb28377cb5d1861eb4103e8867333
Mkhs <jonbecher@microagenetworks.com>
2024-04-26 06:10:57 -0400

    Testing fake data with react charjs 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MitChart from '../components/ui/timelinechart';
 
 export default function Home() {
 
@@ -12,6 +13,7 @@ export default function Home() {
         <p>
           FFXIV Raid Planner
         </p>
+        <MitChart></MitChart>
       </div>
       
     
